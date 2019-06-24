8296948a59162f4bff15d69923312f162788601f
unknown <dannyfsociety@gmail.com>
2019-06-24 17:01:49 -0400

    adding some posts to a specific forum plus login error wa fixed:v:

--- DnlForums/Controllers/PostController.cs
+++ DnlForums/Controllers/PostController.cs
@@ -6,6 +6,7 @@ using DnlForums.Models.Post;
 using DnlForums.Models.Reply;
 using DnlForumsData;
 using DnlForumsData.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DnlForums.Controllers
@@ -13,10 +14,14 @@ namespace DnlForums.Controllers
     public class PostController : Controller
     {
         private readonly IPost postService;
+        private readonly IForum forumService;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public PostController(IPost postService)
+        public PostController(IPost postService, IForum forumService, UserManager<ApplicationUser> userManager) 
         {
             this.postService = postService;
+            this.forumService = forumService;
+            this.userManager = userManager;
         }
         public IActionResult Index(int id)
         {
@@ -49,6 +54,44 @@ namespace DnlForums.Controllers
                 Created = reply.Created,
                 ReplyContent = reply.Content
             });
+        }
+
+        public IActionResult Create(int id)
+        {
+            var forum = this.forumService.GetById(id);
+            var model = new NewPostModel()
+            {
+                ForumId = forum.Id,
+                ForumName = forum.Title,
+                ForumImageUrl = forum.ImageUrl,
+                AuthorName = User.Identity.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost(NewPostModel model)
+        {
+            var userId = this.userManager.GetUserId(User);
+            var user = await this.userManager.FindByIdAsync(userId);
+
+            var post = this.BuildPost(model, user);
+            await this.postService.Add(post);
+
+            return RedirectToAction("Index", "Post", post.Id);
+        }
+
+        private Post BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            var forum = this.forumService.GetById(model.ForumId);
+            return new Post()
+            {
+                Title = model.Title,
+                Content = model.Content,
+                Created = DateTime.Now,
+                User = user,
+                Forum = forum
+            };
         }
     }
 }
